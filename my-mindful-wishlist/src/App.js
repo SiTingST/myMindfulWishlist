c03410fdf136bde4a154326270f18e231d36bd41
SiTingST <tinglim89@gmail.com>
2024-10-20 13:40:43 +0800

    added banner

--- my-mindful-wishlist/src/App.js
+++ my-mindful-wishlist/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import HeaderComponent from './components/headerComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent /> 
     </div>
+
   );
 }
 
