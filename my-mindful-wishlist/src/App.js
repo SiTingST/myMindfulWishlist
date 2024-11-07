325d2cf6c3c88f244e8da0c41044bc1e4a4ae009
SiTingST <tinglim89@gmail.com>
2024-11-07 18:18:00 +0800

    Added editing of items functionality

--- my-mindful-wishlist/src/App.js
+++ my-mindful-wishlist/src/App.js
@@ -9,8 +9,10 @@ import Form from './components/Form.js';
 
 function App() {
   const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
   const [totalCost, setTotalCost] = useState(0);
   const [totalCardsCount, setTotalCardsCount] = useState(0);
+  const [selectedCardData, setSelectedCardData] = useState(null)
 
   const [cards, setCards] = useState(() => {
     const storedCards = localStorage.getItem('items');
@@ -29,14 +31,33 @@ function App() {
   }, [cards]);
 
   const handleAddItems = (cardData) => {
-    setCards([cardData, ...cards]);
-    setTotalCardsCount(totalCardsCount + 1)
+    if (selectedCardData) {
+      setCards((prev) => {
+        return prev.map(card =>
+          card.id === selectedCardData.id ? { ...card, ...cardData } : {...card}
+        )
+      })
+    } else {
+      setCards([cardData, ...cards]);
+      setTotalCardsCount(totalCardsCount + 1)
+    }
     handleFormVisibility()
   };
 
   const handleFormVisibility = () => {
-    setIsFormVisible(prev => !prev);
+    setIsFormVisible(prev => !prev)
+    if (selectedCardData) {
+      setSelectedCardData(null)
+    }
   };
+
+  const handleSetEditMode = () => {
+    setIsEditMode(prev => true);
+  }
+  const handleCardClick = () => {
+    handleFormVisibility()
+    setIsEditMode(false)
+  }
 
   const removeItem = (id) => {
     const updatedCards = cards.filter(item => item.id !== id);
@@ -44,14 +65,21 @@ function App() {
     setCards(updatedCards);
   };
 
+  const setSelectedCardDatas = (data) => {
+    setSelectedCardData(data)
+  }
+
   return (
     <div class="App">
       <HeaderComponent totalCost={totalCost} />
-      {isFormVisible && <Form totalCardCount= {totalCardsCount} handleAddItems={handleAddItems} handleFormVisibility={handleFormVisibility} />}
+      {isFormVisible && <Form totalCardCount= {totalCardsCount} handleAddItems={handleAddItems} handleFormVisibility={handleFormVisibility} isEditMode = {isEditMode} selectedCardData ={selectedCardData} />}
       <div class='flex flex-row flex-wrap'>
-        <AddItemCard handleCardClick ={handleFormVisibility}/>
+        <AddItemCard handleCardClick ={handleCardClick}/>
         {cards.map((card) => (
           <ProductCard 
+            handleFormVisibility={handleFormVisibility}
+            handleSetEditMode = {handleSetEditMode}
+            setSelectedCardData = {setSelectedCardDatas}
             {...card}
             removeItem = {removeItem} />
         ))}
--- my-mindful-wishlist/src/components/Form.js
+++ my-mindful-wishlist/src/components/Form.js
@@ -1,60 +1,64 @@
 
-import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import Tooltip from '@mui/material/Tooltip';
 
 const Form = (props) => {
-    const nameRef = useRef();
-    const priceRef = useRef();
-    const websiteLinkRef = useRef();
-    const imageLinkRef = useRef();
-    const notesRef = useRef();
+    const [formData, setFormData] = useState({
+        name: props?.selectedCardData?.name || '',
+        price: props?.selectedCardData?.price || '',
+        websiteLink: props?.selectedCardData?.websiteLink || '',
+        phone: props?.selectedCardData?.phone || '',
+        imageLink: props?.selectedCardData?.imageLink || '',
+        notes: props?.selectedCardData?.notes || ''
+      });
 
-
-    // add x button
     const handleSubmit = (e) => {
-        e.preventDefault();
-
+        e.preventDefault(); 
         const today = new Date();
         const options = { day: '2-digit', month: 'short', year: 'numeric' };
         const formattedDate = today.toLocaleDateString('en-GB', options);
-        console.log("props.totalCardCount", props.totalCardCount)
+
         const cardData = {
-            id: props.totalCardCount + 1,
-            name: nameRef?.current?.value,
-            price: priceRef?.current?.value,
-            websiteLink: websiteLinkRef?.current?.value,
-            imageLink: imageLinkRef?.current?.value,
-            notes: notesRef?.current?.value,
+            id: props.totalCardCount + 1, 
+            ...formData,
             date: formattedDate,
         };
         props.handleAddItems(cardData);
     };
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+          ...prevState,
+          [name]: value,
+        }));
+      };
+
     return (
         <div class="relative z-10" aria-modal="true" >
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
                 <div class=" max-w-[50%] w-full mx-auto flex items-center p-8 justify-center rounded-xl border border-slate-100 bg-white" >
-                    <form class="w-full" onSubmit={handleSubmit}>
+                    <form class="w-full" onSubmit={handleSubmit}> 
                         <h1 class="text-xl font-medium mb-6"> Add an item </h1>
                         <div class="mb-6">
                             <label for="productName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Name
                                 <span class="text-red-500">*</span>
                             </label>
-                            <input type="text" id="productName" ref={nameRef} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='Enter item name' required />
+                            <input type="text" id="productName" name = "name" value={formData.name} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='Enter item name' onChange={handleChange} required />
                         </div>
                         <div class="mb-6">
                             <label for="productPrice" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Price ($)
                             </label>
-                            <input type="text" id="productPrice" ref={priceRef} pattern="^\d+(\.\d+)?$" title="Enter digits only" class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='50'/>
+                            <input type="text" id="productPrice" name="price" value={formData.price} pattern="^\d+(\.\d+)?$" title="Enter digits only" class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" onChange={handleChange} placeholder='50'/>
                         </div>
                         <div class="mb-6">
                             <label for="websiteLink" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Website Link
                             </label>
-                            <input type="text" id="websiteLink" ref={websiteLinkRef} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='https://www.' />
+                            <input type="text" id="websiteLink" name="websiteLink" value={formData.websiteLink} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" onChange={handleChange}placeholder='https://www.' />
                         </div>
                         <div class="mb-6">
                             <label for="Image Link" class="flex items-center mb-2 pr-6 text-sm font-medium text-gray-900 dark:text-white">
@@ -63,17 +67,17 @@ const Form = (props) => {
                                     <HelpOutlineIcon className="ml-0.5" fontSize="small" />
                                 </Tooltip>
                             </label>
-                            <input type="text" id="imageLink" ref={imageLinkRef} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='https://www.' />
+                            <input type="text" id="imageLink" name="imageLink" value={formData.imageLink} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='https://www.' />
                         </div>
                         <div class="mb-6">
                             <label for="notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Notes
                             </label>
-                            <input type="text" id="notes" ref={notesRef} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='I love the design!' />
+                            <input type="text" id="notes" name="notes" value={formData.notes} class="border block w-full p-2.5 text-sm rounded-lg focus:outline-none focus:shadow-lg" placeholder='I love the design!' />
                         </div>
                         <div class="flex justify-end gap-4">
                             <button type="submit" class=" bg-green-700 hover:shadow-lg text-white h-10 px-6 rounded-full">
-                                Add Item
+                                {props.isEditMode ? 'Save Changes' : 'Add Items' }
                             </button>
                             <button class=" bg-black hover:shadow-lg text-white h-10 px-6 rounded-full" onClick={props.handleFormVisibility} >
                                 Cancel
